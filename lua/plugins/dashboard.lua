5a9e267103feef02deb433839802dfa65507ad62
Geovane Clemente de Oliveira <geovane@maptriz.com.br>
2022-09-23 08:16:52 -0300

    config trampo

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,3 +1,38 @@
+local home = os.getenv('HOME')
+local db = require('dashboard')
+-- db.preview_command = 'cat | lolcat -F 0.3'
+-- db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+db.preview_file_height = 12
+db.preview_file_width = 80
+db.custom_center = {
+    {icon = '  ',
+    desc = 'Recently latest session                  ',
+    shortcut = 'SPC s l',
+    action ='SessionLoad'},
+    {icon = '  ',
+    desc = 'Recently opened files                   ',
+    action =  'DashboardFindHistory',
+    shortcut = 'SPC f h'},
+    {icon = '  ',
+    desc = 'Find  File                              ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'},
+    {icon = '  ',
+    desc ='File Browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'},
+    {icon = '  ',
+    desc = 'Find  word                              ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'},
+    {icon = '  ',
+    desc = 'Open Personal dotfiles                  ',
+    action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+    shortcut = 'SPC f d'},
+  }
+
+
+-- arrumar isso
 vim.g.dashboard_custom_header = {
   ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
   ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
@@ -11,5 +46,7 @@ vim.g.dashboard_custom_section = {
     a = {description = {'  File Browser       '}, command = 'Telescope find_files'},
     d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
     b = {description = {'  Recent             '}, command = 'Telescope oldfiles'},
+    f = {description = {'  Git Status         '}, command = 'Telescope git_status'}
 }
+
 
